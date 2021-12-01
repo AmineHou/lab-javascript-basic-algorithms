1965828708babf8efdf8679131460d1b62eb781a
Amine <aminehouidi90@gmail.com>
2021-12-01 16:54:11 +0100

    iteration 3.1 - done

--- js/index.js
+++ js/index.js
@@ -9,12 +9,23 @@ console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
 
 if (hacker1.length > hacker2.length) {
-  console.log(
-    `The driver has the longest name, it has ${hacker1.length} characters.`
-  );
+    console.log(
+        `The driver has the longest name, it has ${hacker1.length} characters.`
+    );
 } else {
-  console.log(
-    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
-  )};
+    console.log(
+        `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+    )
+};
 
 // Iteration 3: Loops
+
+let hacker1String = ""
+
+for (let i = 0; i < hacker1.length; i++) {
+    let hacker1Loop = hacker1.charAt(i)
+
+    hacker1String = hacker1String + hacker1Loop.toUpperCase() + " "
+    // === hacker1String += hacker1Loop.toUpperCase() + " "
+}
+console.log("i get", hacker1String);
